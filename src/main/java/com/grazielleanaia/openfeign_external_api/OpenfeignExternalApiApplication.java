ce55ea1e6b040e3a51829e56e534511cfa464bab
Grazielle <grazielle.anaia@gmail.com>
2025-05-19 11:57:09 -0400

    Initial commit

---
+++ src/main/java/com/grazielleanaia/openfeign_external_api/OpenfeignExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.openfeign_external_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignExternalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfeignExternalApiApplication.class, args);
+	}
+
+}
